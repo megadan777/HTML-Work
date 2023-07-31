c034877d85d1c697814fc1be63759004de76f5c1
megadan777 <daniel.mowbray86@gmail.com>
2023-07-31 21:13:04 +1000

    Java

--- Java/javascript.js
+++ Java/javascript.js
@@ -29,11 +29,22 @@ let message = 'Hello world!';
 message = hello;
 
 alert(hello);
-alert(message);*/
+alert(message);
 
 //Example 5
 
 let $ = 1;
 let _ = 49;
 
-alert($ + _);
+alert($ + _);*/
+
+//Example 6
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color);
