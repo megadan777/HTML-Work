53551d575ce7fc7a6ff59552c6b97baa88314423
megadan777 <daniel.mowbray86@gmail.com>
2023-07-31 20:32:57 +1000

    Java

--- Java/javascript.js
+++ Java/javascript.js
@@ -1,6 +1,6 @@
 /*let message = 'Hello';
 
-alert(message); */
+alert(message); 
 
 
 //Example 2
@@ -8,3 +8,25 @@ alert(message); */
 let user = 'John',
   age = 25,
   message = 'Hello';
+
+
+  //Example 3
+
+  let message;
+
+message = 'Hello!';
+
+message = 'World!';
+
+alert(message);*/
+
+//Example 4
+
+let hello = 'Hello world!';
+
+let message = 'Hello world!';
+
+message = hello;
+
+alert(hello);
+alert(message);
