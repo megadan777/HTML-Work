8ff9c62fc6d94839f0277d8b361813e2899ff316
megadan777 <daniel.mowbray86@gmail.com>
2023-09-05 20:39:23 +1000

    JavaScript

---
+++ JavaScript/javascript.js
@@ -0,0 +1,51 @@
+/*let message = 'Hello';
+
+alert(message); 
+
+
+//Example 2
+
+let user = 'John',
+  age = 25,
+  message = 'Hello';
+
+
+  //Example 3
+
+  let message;
+
+message = 'Hello!';
+
+message = 'World!';
+
+alert(message);
+
+//Example 4
+
+let hello = 'Hello world!';
+
+let message = 'Hello world!';
+
+message = hello;
+
+alert(hello);
+alert(message);
+
+//Example 5
+
+let $ = 1;
+let _ = 49;
+
+alert($ + _);
+
+//Example 6
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color);*/
+
---
+++ JavaScript/javascript2.js
@@ -0,0 +1,18 @@
+/*let = admin;
+let = firstname;
+
+firstname = 'John';
+
+admin = firstname;
+
+alert( admin );
+
+let = planetEarth
+
+let = CurrentVisitor */
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+alert(age);
