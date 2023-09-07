25d981b03d865a97e15c2265e35ed10589069d0e
megadan777 <daniel.mowbray86@gmail.com>
2023-09-07 22:08:29 +1000

    RockPaperScissors

---
+++ JavaScript/RockPaperScissors/rps-game.js
@@ -0,0 +1,28 @@
+/*
+Learning how to write JS that lets me play a 5rd game against the pc of Rock Paper Scissors.
+
+1. The user can input rock, paper, scissors that is not case sensitive
+2. A function needs to be created that allows the computer to make a choice
+3. The result for user/computer winner will be displayed.
+4. Tie rounds need to be counted in code
+5. These steps will be repeated for 5 rounds.
+6. All permeatations need to be factored into if/else statements
+
+*/
+
+
+
+
+function getComputerChoice{
+const computerSelection = "rock"
+const computerSelection = "paper"
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
