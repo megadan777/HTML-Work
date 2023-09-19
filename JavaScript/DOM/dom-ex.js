4b10d566ccb2c1e6fbbae4fa1a7e447ddbf91e90
megadan777 <daniel.mowbray86@gmail.com>
2023-09-19 21:36:11 +1000

    DOM

--- JavaScript/DOM/dom-ex.js
+++ JavaScript/DOM/dom-ex.js
@@ -4,12 +4,26 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
+const red = document.createElement("p")
+red.style.color = "red";
+red.textContent = 'Hey I’m red!';
 
-const red = document.querySelector('#red');
-
-document.getElementById("red").style.color = "red";
-content.classList.add('content');
-content.textContent = 'Hey I’m red!';
+const blue = document.createElement("h3")
+blue.style.color = "blue";
+blue.textContent = 'I’m a blue h3!';
 
 container.appendChild(content);
-red.appendChild(content);
+container.appendChild(red);
+container.appendChild(blue);
+
+const myDiv = document.createElement("div"); 
+myDiv.setAttribute('style', 'border: black solid; background-color: pink;'); 
+
+    const anotherH = document.createElement("h1"); 
+    anotherH.textContent = "I'm in a div"; 
+    const anotherP = document.createElement("p"); 
+    anotherP.textContent = "ME TOO!"; 
+
+    myDiv.appendChild(anotherH);
+    myDiv.appendChild(anotherP);
+    container.appendChild(myDiv);
