46f307f0a1bc0a54369e4f6fc6be730afdbb6a05
megadan777 <daniel.mowbray86@gmail.com>
2023-10-02 22:04:42 +1100

    Etch

--- Projects/Etch/etch.js
+++ Projects/Etch/etch.js
@@ -22,3 +22,14 @@ for (let i = 1; i <= celltot; i++) {
   }
   gridContainer.appendChild(cell);
 }
+
+
+var color = [, "#3C9EE7", "#E7993C",
+			"#E73C99", "#3CE746", "#E7993C"];
+
+document.querySelector("div").addEventListener(
+	"mouseover", function () {
+
+		document.querySelector("div").style.background
+			= color[(Math.floor(Math.random() * color.length))];
+	})
